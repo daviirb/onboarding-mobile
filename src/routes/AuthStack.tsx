957cd5bf396b3e132e245b10e03a1abf22c906a2
daviirb <davi.rb@hotmail.com>
2024-07-08 07:57:42 -0300

    feat: conditional navigation

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -2,7 +2,12 @@ import { LoginScreen } from "@/screens/auth/LoginScreen";
 import { SignUpScreen } from "@/screens/auth/SignUpScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
+export type AuthParamList = {
+  LoginScreen: undefined;
+  SignUpScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<AuthParamList>();
 
 export function AuthStack() {
   return (
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,45 +1,31 @@
-import { LoginScreen } from "@/screens/auth/LoginScreen";
-import { SignUpScreen } from "@/screens/auth/SignUpScreen";
-import { OnboardingScreen1 } from "@/screens/onboarding/OnboardingScreen1";
-import { OnboardingScreen2 } from "@/screens/onboarding/OnboardingScreen2";
-import { OnboardingScreen3 } from "@/screens/onboarding/OnboardingScreen3";
+import { OnboardingProvider, useOnboarding } from "@/contexts/AuthContext";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useState } from "react";
+import { AuthStack } from "./AuthStack";
+import { OnboardingStack } from "./OnboardingStack";
 
-export function Router() {
-  const [hasCompletedOnboarding, setHasCompletedOnboarding] = useState(false);
-  const Stack = createNativeStackNavigator();
+export type RouteParamList = {
+  Auth: undefined;
+  Onboarding: undefined;
+};
+
+function AppNavigator() {
+  const { hasCompletedOnboarding, isLoading } = useOnboarding();
+
+  if (isLoading) {
+    return null; // Ou um componente de carregamento
+  }
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          fullScreenGestureEnabled: true,
-        }}
-        initialRouteName="OnboardingScreen1"
-      >
-        <>
-          <Stack.Screen
-            name="OnboardingScreen1"
-            component={OnboardingScreen1}
-          />
-          <Stack.Screen
-            name="OnboardingScreen2"
-            component={OnboardingScreen2}
-          />
-          <Stack.Screen
-            name="OnboardingScreen3"
-            component={OnboardingScreen3}
-          />
-        </>
-
-        <>
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-        </>
-      </Stack.Navigator>
+      {hasCompletedOnboarding ? <AuthStack /> : <OnboardingStack />}
     </NavigationContainer>
+  );
+}
+
+export function Router() {
+  return (
+    <OnboardingProvider>
+      <AppNavigator />
+    </OnboardingProvider>
   );
 }
