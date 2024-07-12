42848a3088b63f713d34e7d554fa8ac73bce466b
daviirb <davi.rb@hotmail.com>
2024-07-12 07:05:56 -0300

    feat: Add conditional splash screen

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,8 @@
+import Logo from "@/components/Logo/Logo";
 import { OnboardingProvider, useOnboarding } from "@/contexts/AuthContext";
+import { useDynamicStyles } from "@/styles/styles";
 import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
 import { AuthStack } from "./AuthStack";
 import { OnboardingStack } from "./OnboardingStack";
 
@@ -10,9 +13,14 @@ export type RouteParamList = {
 
 function AppNavigator() {
   const { hasCompletedOnboarding, isLoading } = useOnboarding();
+  const { containerSplash, dynamicLogo } = useDynamicStyles();
 
   if (isLoading) {
-    return null;
+    return (
+      <View style={containerSplash}>
+        <Logo height={97} width={150} color={dynamicLogo.color} />
+      </View>
+    );
   }
 
   return (
